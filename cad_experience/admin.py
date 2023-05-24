f487ac5dfad1e2f86930de2f37ab63477a2ffb54
Paulo Junior <npaulo.ce@gmail.com>
2023-05-24 17:13:07 -0300

    Full complete config CAD_EXPERIENCE

--- cad_experience/admin.py
+++ cad_experience/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CadExperience
 
-# Register your models here.
+class ListCadExperience(admin.ModelAdmin):
+    list_display = ('cadexperience_id', 'cadexperience_first_name', 'cadexperience_last_name', 'cadexperience_degree')
+
+admin.site.register(CadExperience, ListCadExperience)
